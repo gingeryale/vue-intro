8c64747d07fadc416c363a65cbd935d4695d8b21
ao <aolin@ymail.com>
2023-02-02 14:16:46 +0200

    filter by  tag

--- js/components/AssignmentList.js
+++ js/components/AssignmentList.js
@@ -7,11 +7,15 @@ export default {
     <h2 class="font-bold mb-2" >{{title}}, <span>{{assignments.length}}</span></h2>
     <div class="flex gap-2">
     <button @click="currentTag = tag"
-    v-for="tag in tags" class="border rounded p-1 text-xs">{{tag}}</button>
+    v-for="tag in tags" 
+    class="border rounded p-1 text-xs"
+    :class="{'border-blue-500 text-blue-500':tag== currentTag
+    }"
+    >{{tag}}</button>
     </div>
     <ul class="border border-gray-600 p-3 divide-y divide-gray-600 mt-5">
         <assignment 
-        v-for="assignment in assignments" 
+        v-for="assignment in filteredAssignments" 
         :key="assignment.id"
         :assignment="assignment">
            
@@ -25,12 +29,18 @@ export default {
     },
     data() {
         return {
-            currentTag: ''
+            currentTag: 'all'
         };
     },
     computed: {
+        filteredAssignments() {
+            if (this.currentTag == 'all') {
+                return this.assignments;
+            }
+            return this.assignments.filter(a => a.tag === this.currentTag);
+        },
         tags() {
-            return new Set(this.assignments.map(a => a.tag));
+            return ['all', ... new Set(this.assignments.map(a => a.tag))];
         }
     }
 }
